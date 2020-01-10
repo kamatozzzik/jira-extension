9fe4027c07454cc5705844393aa481733e22f779
Uladzislau Yavarouski <u.yavarouski@itechart-group.com>
2020-01-10 12:00:44 +0300

    add build to project

---
+++ build/precache-manifest.6b0856ff3a03cc6c27398d3dab656041.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1be01269cdcc614bc69414740ad56a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67e7c23f693c12d9c7b",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "f67e7c23f693c12d9c7b",
+    "url": "/static/js/main.js"
+  }
+]);
